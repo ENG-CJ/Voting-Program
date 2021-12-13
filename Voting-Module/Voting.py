1f49e48599582e3224292f54d101096fc694e41a
Abdulrahman-Haji <77126546+ENG-CJ@users.noreply.github.com>
2021-12-13 20:31:31 +0300

    Add files via upload

---
+++ Voting-Module/Voting.py
@@ -0,0 +1,18 @@
+
+#  Functions
+
+def header():
+    print('Voting Program')
+    print('Creator: Eng-Cj')
+    print('---------------------')
+    return
+
+def validate():
+    print('------ONLY SOMALI PPLE CAN VOTE')
+
+def candidates():
+    print('1: Mohamed Farmaajo')
+    print('2: Hassan Ali Kheyre')
+    print('3: Hassan Shiekh')
+    print('3: Shiekh Sharif')
+    return
